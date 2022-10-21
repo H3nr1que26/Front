07e7338266280a3d44ef1847a20de58e69ab8e05
Luca Rodrigues <74923223+LucaRodrigues@users.noreply.github.com>
2022-10-20 21:44:47 -0300

    Add files via upload

--- views/Salas.js
+++ views/Salas.js
@@ -1,20 +1,25 @@
 import React, {useState,useEffect} from 'react';
 import { KeyboardAvoidingView, TextInput, TouchableOpacity, Image, Text, View, StyleSheet} from 'react-native';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 import Style from '../assets/css/Css'
 
-export default function Salas()
-{
-const [erroLogin, setErroLogin] = useState(0);
+        export default function ColumnsGrid() {
+                    return ColumnsGrid()(          
+                    <Box sx={{ flexGrow: 1 }}>
+                        <text><h1>Suas Salas</h1></text>
+                        <Grid container spacing={2} columns={16}>
+                        <Grid item xs={8}>
+                            <Item>xs=8</Item>
+                        </Grid>
+                        <Grid item xs={8}>
+                            <Item>xs=8</Item>
+                        </Grid>
+                        </Grid>
+                    </Box>
+                    );
+                }
 
-    return(
-        <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={Style.container}>
-            <View style={Style.login__logomarca}>
-                <Image source={require('../assets/img/logomarca.png')} />
-            </View>
-            <text><h1>Suas Salas</h1></text>
-            
-        </KeyboardAvoidingView>
-    );
-}
-
-
+                
